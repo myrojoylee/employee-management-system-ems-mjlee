91fd2ba5494fd5ed46560d69b7314b9faf838361
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-05-31 09:31:39 -0400

    added more inquirer prompts

--- index.js
+++ index.js
@@ -31,6 +31,83 @@ const addDepartmentPrompt = [
   },
 ];
 
+const addEmployeePrompt = [
+  {
+    type: "input",
+    name: "first_name",
+    message: "What is the employee's first name? ",
+  },
+  {
+    type: "input",
+    name: "last_name",
+    message: "What is the employee's last name? ",
+  },
+  {
+    type: "list",
+    name: "employee_role",
+    choices: [
+      "Sales Lead",
+      "Salesperson",
+      "Lead Engineer",
+      "Software Engineer",
+      "Account Manager",
+      "Accountant",
+      "Legal Team Lead",
+      "Legal Team Associate",
+    ],
+  },
+];
+
+const addRolePrompt = [
+  {
+    type: "input",
+    name: "new_role",
+    message: "What is the name of the new role? ",
+  },
+  {
+    type: "input",
+    name: "salary",
+    message:
+      "What is the salary for this role? (DO NOT insert commas or any currency symbols) ",
+  },
+  {
+    type: "list",
+    name: "department",
+    message: "Which department does this role belong to? ",
+    choices: ["Engineering", "Finance", "Legal", "Sales"],
+  },
+  {
+    type: "list",
+    name: "manager",
+    message: "Who is the employee's manager? ",
+    choices: ["Engineering", "Finance", "Legal", "Sales"],
+  },
+];
+
+const updateEmployeePrompt = [
+  {
+    type: "list",
+    name: "update_employee",
+    message: "Choose the employee below you would like to update: ",
+    choices: ["Jane Doe", "John Doe", "John Smith"],
+  },
+  {
+    type: "list",
+    name: "update_role",
+    message: "Which role do you want to assign to the selected employee? ",
+    choices: [
+      "Sales Lead",
+      "Salesperson",
+      "Lead Engineer",
+      "Software Engineer",
+      "Account Manager",
+      "Accountant",
+      "Legal Team Lead",
+      "Legal Team Associate",
+    ],
+  },
+];
+
 function init() {
   inquirer
     .prompt(main_menu)
@@ -43,8 +120,11 @@ function nextStep(main_menu_answer) {
   let reply;
   switch (main_menu_answer.main_menu) {
     case "View All Employees":
-      reply = `we see all employees`;
+      viewAllEmployees();
       console.log(reply);
+      break;
+    case "View All Roles":
+      viewAllRoles();
       break;
     case "View All Departments":
       viewAllDepartments();
@@ -54,15 +134,29 @@ function nextStep(main_menu_answer) {
         .prompt(addDepartmentPrompt)
         .then((new_department_answer) => addDepartment(new_department_answer));
       break;
-    default:
-      reply = `i guess we see something else`;
-      console.log(reply);
+    case "Add Employee":
+      inquirer
+        .prompt(addEmployeePrompt)
+        .then((new_employee_answers) => addEmployee(new_employee_answers));
+      break;
+    case "Add Role":
+      inquirer
+        .prompt(addRolePrompt)
+        .then((new_role_answers) => addRole(new_role_answers));
+      break;
+    case "Update Employee Role":
+      inquirer
+        .prompt(updateEmployeePrompt)
+        .then((update_employee_answers) =>
+          updateEmployee(update_employee_answers)
+        );
   }
 }
 
 function viewAllDepartments() {
+  let queryStatement = `SELECT * FROM departments`;
   db.promise()
-    .query("SELECT * FROM departments")
+    .query(queryStatement)
     .then(([rows]) => {
       console.log(rows);
     })
@@ -82,8 +176,79 @@ function addDepartment(new_department_answer) {
     .then(() => db.end());
 }
 
-function addEmployee() {}
+function viewAllEmployees() {
+  let queryStatement = `SELECT * FROM employees`;
+  db.promise()
+    .query(queryStatement)
+    .then(([rows]) => {
+      console.log(rows);
+    })
+    .catch(console.log)
+    .then(() => db.end());
+}
 
-function updateEmployee() {}
+function addEmployee() {
+  let queryStatement = `INSERT INTO employee (first_name, last_name, role_id ) VALUES ('${new_department_answer.department_name}')`;
+  db.promise()
+    .query(queryStatement)
+    .then(([rows]) => {
+      console.log(`Departments have been updated`);
+      return rows;
+    })
+    .catch(console.log)
+    .then(() => db.end());
 
-function addRole() {}
+  console.log(`Added first name last name to the database`);
+}
+// fix
+function updateEmployee() {
+  let queryStatement = `UPDATE employee (first_name, last_name, role_id ) VALUES ('${new_department_answer.department_name}')`;
+  db.promise()
+    .query(queryStatement)
+    .then(([rows]) => {
+      console.log(`Departments have been updated`);
+      return rows;
+    })
+    .catch(console.log)
+    .then(() => db.end());
+
+  console.log(`Updated employee's role`);
+}
+
+function viewAllRoles() {
+  let queryStatement = `SELECT * FROM roles`;
+  db.promise()
+    .query(queryStatement)
+    .then(([rows]) => {
+      console.log(rows);
+    })
+    .catch(console.log)
+    .then(() => db.end());
+}
+
+//fix
+function addRole() {
+  let queryStatement = `INSERT INTO role (title, salary, deptartment_id ) VALUES ('${new_department_answer.department_name}')`;
+  db.promise()
+    .query(queryStatement)
+    .then(([rows]) => {
+      console.log(`Departments have been updated`);
+      return rows;
+    })
+    .catch(console.log)
+    .then(() => db.end());
+}
+
+function addSalary() {}
+
+//Bonus
+
+// add employee managers
+
+// view employees by manager
+
+// view employees by department
+
+// functionality to delete depts, roles, and employees
+
+// functionality to view total utilized budget of dept - combined salaries of employees in dept
