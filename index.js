e6c6e1e52fdd5d600c1798ab1776b4d42331c3e7
Myro Joy Lee <myro.joy.olida.092282@gmail.com>
2023-06-06 15:19:05 -0400

    moved prompts to a separate lib file

--- index.js
+++ index.js
@@ -2,214 +2,69 @@
 //                 ---- DEPENDENCIES ----            //
 // ================================================= //
 const inquirer = require("inquirer");
-const db = require("./lib/db");
-const createTable = require("./lib/createTable");
-
-// ================================================= //
-//               ---- QUESTION BANKS ----            //
-// ================================================= //
-let currentDeptList = ["Engineering", "Finance", "Legal", "Sales"];
-let currentRoleList = [
-  "Sales Lead",
-  "Salesperson",
-  "Lead Engineer",
-  "Software Engineer",
-  "Account Manager",
-  "Accountant",
-  "Legal Team Lead",
-  "Legal Team Associate",
-];
-let currentEmpList = [
-  "None",
-  "Buzz Lightyear",
-  "Mickey Mouse",
-  "Queen Of Hearts",
-  "Minnie Mouse",
-  "Pluto Planet",
-  "Snow White",
-  "Cruella de Vil",
-  "Randall Boggs",
-];
-const main_menu = [
-  {
-    type: "list",
-    name: "main_menu",
-    message: "What would you like to do? (Scroll and choose 'EXIT' to exit)",
-    choices: [
-      "View All Employees",
-      "Add Employee",
-      "Update Employee Role",
-      "View All Roles",
-      "Add Role",
-      "View All Departments",
-      "Add Department",
-      "Update Employee Managers",
-      "View Employees by Manager",
-      "View Employees by Department",
-      "Delete Department",
-      "Delete Role",
-      "Delete Employee",
-      "EXIT",
-    ],
-  },
-];
-
-const addDepartmentPrompt = [
-  {
-    type: "input",
-    name: "department_name",
-    message: "What is the name of the new department?",
-  },
-];
-
-const addEmployeePrompt = [
-  {
-    type: "input",
-    name: "first_name",
-    message: "What is the employee's first name? ",
-  },
-  {
-    type: "input",
-    name: "last_name",
-    message: "What is the employee's last name? ",
-  },
-  {
-    type: "list",
-    name: "employee_role",
-    message: "What is this employee's role? ",
-    choices: currentRoleList,
-  },
-  {
-    type: "list",
-    name: "manager",
-    message: "Who is the employee's manager? ",
-    choices: currentEmpList,
-  },
-];
-
-const addRolePrompt = [
-  {
-    type: "input",
-    name: "new_role",
-    message: "What is the name of the new role? ",
-  },
-  {
-    type: "input",
-    name: "salary",
-    message: "What is the salary for this role? (example: 100000) ",
-  },
-  {
-    type: "list",
-    name: "department",
-    message: "Which department does this role belong to? ",
-    choices: currentDeptList,
-  },
-];
-
-const updateEmployeePrompt = [
-  {
-    type: "list",
-    name: "update_employee",
-    message: "Choose the employee below you would like to update: ",
-    choices: currentEmpList,
-  },
-  {
-    type: "list",
-    name: "update_role",
-    message: "Which role do you want to assign to the selected employee? ",
-    choices: currentRoleList,
-  },
-];
-
-const updateEmpMgrPrompt = [
-  {
-    type: "list",
-    name: "update_emp_mgr",
-    message: "Choose the employee below you would like to update: ",
-    choices: currentEmpList,
-  },
-  {
-    type: "list",
-    name: "update_role",
-    message: "Which role do you want to assign to the selected employee? ",
-    choices: currentRoleList,
-  },
-];
-
-const deleteDepartmentPrompt = [
-  {
-    type: "list",
-    name: "delete_department",
-    message: "Which department did you want to delete?",
-    choices: currentDeptList,
-  },
-];
-const deleteRolePrompt = [
-  {
-    type: "list",
-    name: "delete_role",
-    message: "Which role did you want to delete?",
-    choices: currentRoleList,
-  },
-];
-const deleteEmployeePrompt = [
-  {
-    type: "list",
-    name: "delete_emp",
-    message: "Which employee did you want to delete?",
-    choices: currentEmpList,
-  },
-];
+const Department = require("./class-lib/department");
+const Role = require("./class-lib/role");
+const Employee = require("./class-lib/employee");
+const Prompt = require("./lib/inquirerPrompts");
+const figlet = require("figlet");
 
 function init() {
-  inquirer
-    .prompt(main_menu)
-    .then((main_menu_answer) => nextStep(main_menu_answer));
+  figlet(`EMPLOYEE\nTRACKER`, function (err, data) {
+    err ? console.error(err) : console.log(data);
+
+    inquirer
+      .prompt(Prompt[3])
+      .then((main_menu_answer) => nextStep(main_menu_answer));
+  });
 }
 
 init();
 
 function nextStep(main_menu_answer) {
+  const dept = new Department();
+  const employee = new Employee();
+  const role = new Role();
   switch (main_menu_answer.main_menu) {
     case "View All Employees":
-      viewAllEmployees();
+      employee.getList(init);
       break;
     case "View All Roles":
-      viewAllRoles();
+      role.getList(init);
       break;
     case "View All Departments":
-      viewAllDepartments();
+      dept.getList(init);
       break;
     case "Add Department":
-      inquirer
-        .prompt(addDepartmentPrompt)
-        .then((new_department_answer) => addDepartment(new_department_answer));
+      inquirer.prompt(Prompt[4]).then((new_department_answer) => {
+        dept.addDept(new_department_answer, init);
+      });
       break;
     case "Add Employee":
       inquirer
-        .prompt(addEmployeePrompt)
-        .then((new_employee_answers) => addEmployee(new_employee_answers));
+        .prompt(Prompt[5])
+        .then((new_employee_answers) =>
+          employee.addEmployee(new_employee_answers, init)
+        );
       break;
     case "Add Role":
       inquirer
-        .prompt(addRolePrompt)
-        .then((new_role_answers) => addRole(new_role_answers));
+        .prompt(Prompt[6])
+        .then((new_role_answers) => role.addRole(new_role_answers, init));
       break;
     case "Update Employee Role":
       inquirer
-        .prompt(updateEmployeePrompt)
+        .prompt(Prompt[7])
         .then((update_employee_answers) =>
-          updateEmployee(update_employee_answers)
+          employee.updateEmployee(update_employee_answers, init)
         );
       break;
     case "Update Employee Managers":
       inquirer
-        .prompt(updateEmpMgrPrompt)
+        .prompt(Prompt[8])
         .then((update_emg_mgr_answers) => addRole(update_emg_mgr_answers));
       break;
     case "View Employees by Manager":
-      inquirer;
-      viewEmployeesByManager();
+      inquirer.prompt(Prompt);
       break;
     case "View Employees by Department":
       viewEmployeesByDept();
@@ -232,150 +87,4 @@ function nextStep(main_menu_answer) {
     case "EXIT":
       process.exit();
   }
-}
-
-function viewAllDepartments() {
-  let queryStatement = `SELECT * FROM departments`;
-  db.promise()
-    .query(queryStatement)
-    .then(([rows]) => {
-      let option = "dept";
-      createTable(rows, option);
-    })
-    .catch(console.log)
-    .then(() => init());
-}
-
-function addDepartment(new_department_answer) {
-  let queryStatement = `INSERT INTO departments (name) VALUES ('${new_department_answer.department_name}')`;
-  db.promise()
-    .query(queryStatement)
-    .then(([rows]) => {
-      currentDeptList.push(new_department_answer.department_name);
-      console.log(
-        `Added ${new_department_answer.department_name} to the database.`
-      );
-    })
-    .catch(console.log)
-    .then(() => init());
-}
-
-/**
- * JOIN between dept, role, and employee tables
- */
-function viewAllEmployees() {
-  let queryStatement = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employees e LEFT JOIN employees m ON m.id = e.manager_id JOIN roles r ON e.role_id = r.id JOIN departments d ON d.id = r.department_id`;
-  db.promise()
-    .query(queryStatement)
-    .then(([rows]) => {
-      let option = "empl";
-      createTable(rows, option);
-    })
-    .catch(console.log)
-    .then(() => init());
-}
-
-function addEmployee(new_employee_answers) {
-  let role_id =
-    currentRoleList.findIndex(
-      (role) => role === new_employee_answers.employee_role
-    ) + 1;
-
-  let manager_id;
-  new_employee_answers.manager != "None"
-    ? (manager_id = currentEmpList.findIndex(
-        (emp) => emp === new_employee_answers.manager
-      ))
-    : (manager_id = "null");
-
-  let queryStatement = `INSERT INTO employees (first_name, last_name, role_id, manager_id ) VALUES ('${new_employee_answers.first_name}', '${new_employee_answers.last_name}', ${role_id}, ${manager_id})`;
-  db.promise()
-    .query(queryStatement)
-    .then(([rows]) => {
-      let fullName = `${new_employee_answers.first_name} ${new_employee_answers.last_name}`;
-      currentEmpList.push(fullName);
-      console.log(`Added ${fullName} to the database`);
-    })
-    .catch(console.log)
-    .then(() => init());
-}
-
-function updateEmployee(update_employee_answers) {
-  let role_id =
-    currentRoleList.findIndex(
-      (emp) => emp === update_employee_answers.update_role
-    ) + 1;
-  let first_name = update_employee_answers.update_employee.split(" ")[0];
-  let last_name = update_employee_answers.update_employee.split(" ")[1];
-  let queryStatement = `UPDATE employees SET first_name = "${first_name}", last_name = "${last_name}", role_id = ${role_id} WHERE first_name = "${first_name}"`;
-  db.promise()
-    .query(queryStatement)
-    .then(([rows]) => {
-      console.log(`Updated ${update_employee_answers.update_employee}'s role`);
-    })
-    .catch(console.log)
-    .then(() => init());
-}
-
-/**
- * JOIN between dept and role tables
- */
-function viewAllRoles() {
-  let queryStatement = `SELECT r.id, r.title, d.name AS department, r.salary FROM departments d JOIN roles r ON r.department_id = d.id`;
-  db.promise()
-    .query(queryStatement)
-    .then(([rows]) => {
-      let option = "role";
-      createTable(rows, option);
-    })
-    .then(() => init());
-}
-
-//fix
-function addRole(new_role_answers) {
-  currentRoleList.push(new_role_answers.new_role);
-  let department_id =
-    currentDeptList.findIndex((dept) => dept === new_role_answers.department) +
-    1;
-  let queryStatement = `INSERT INTO roles (title, department_id, salary) VALUES ('${new_role_answers.new_role}', ${department_id}, ${new_role_answers.salary})`;
-  db.promise()
-    .query(queryStatement)
-    .then(([rows]) => {
-      console.log(`Added ${new_role_answers.new_role} to the database.`);
-    })
-    .catch(console.log)
-    .then(() => init());
-}
-
-// =-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=
-// bonus material
-
-function updateEmployeeManager(update_emg_mgr_answers) {
-  console.log(update_emg_mgr_answers);
-  process.exit();
-}
-
-function viewEmployeesByManager() {
-  console.log(`insert query to view employees by manager`);
-  process.exit();
-}
-
-function viewEmployeesByDept() {
-  console.log(`insert query to view employees by dept`);
-  process.exit();
-}
-
-function deleteDept(delete_dept_answers) {
-  console.log(delete_dept_answers);
-  process.exit();
-}
-
-function deleteRole(delete_role_answers) {
-  console.log(delete_role_answers);
-  process.exit();
-}
-
-function deleteEmployee(delete_emp_answers) {
-  console.log(delete_emp_answers);
-  process.exit();
 }
